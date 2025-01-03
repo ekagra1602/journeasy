8d112ebac308f932015c1f2de720c16d6b91c728
Ekagra Gupta <egupta3@asu.edu>
2025-01-02 22:02:26 -0700

    Added navbar and footer

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-500 p-4 text-white">
+      <h1 className="text-2xl">Journeasy</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
