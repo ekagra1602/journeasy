fcf02f6f7df9c4d911eabf07db313e12b1f5ac8f
Ekagra Gupta <egupta3@asu.edu>
2025-01-23 23:55:11 -0700

    Backend

--- api/db.js
+++ api/db.js
@@ -11,7 +11,7 @@ async function connectDB() {
         return cached.conn;
     }
 
-    if (!cached.promise) {
+    if (!cached.promis) {
         cached.promise = mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
